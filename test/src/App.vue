e297e87f911d4f83bee17c4e3da1fcbb3cc36cef
josephJaoNasser <57519561+josephJaoNasser@users.noreply.github.com>
2020-07-25 18:32:14 +0800

    Folder transfer

---
+++ test/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <!-- 
+      
+      v-bind syntax:
+
+        v-bind:(name of the thing you wanna pass) 
+            ="(the data in the script you wanna bind)"
+
+        example:
+
+        v-bind:todos = "todos_arr"
+
+    -->
+    <Todos v-bind:todos="todos_arr"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data(){
+    return{
+      todos_arr: [
+      {
+        id: 1,
+        title: 'ey',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'yo',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'aye',
+        completed: true
+      }
+    ]}
+  }
+}
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+  }
+</style>
---
+++ test/src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main-div">
+        <div v-bind:key="t.id" v-for="t in todos">
+            <h3>{{t.title}}</h3>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:"Todos",
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
